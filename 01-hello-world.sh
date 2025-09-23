b96da526b5049de47e79dcaad3dae38077f9ccc2
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-24 02:35:21 +0530

    some message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "hello world!"
+echo "ramesh:: hi suresh, how are you ?"
+echo "suresh:: hello ramesh, I am fine. How are you?"
+echo "ramesh:: what are you learing recently?"
+echo "suresh:: I am learning shell scripting ramesh"
+
