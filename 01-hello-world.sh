6a03a4c4b4cfee056aa8761ace69b5fdab406b31
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-24 22:19:18 +0530

    variable

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "ramesh:: hi suresh, how are you ?"
-echo "suresh:: hello ramesh, I am fine. How are you?"
-echo "ramesh:: what are you learing recently?"
-echo "suresh:: I am learning shell scripting ramesh"
 
