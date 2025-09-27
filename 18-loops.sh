8c2758027b8756dff994535decd9c465dc46e678
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-27 14:01:26 +0530

    variable

--- 18-loops.sh
+++ 18-loops.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
@@ -28,16 +29,17 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 }
 
 # $@
+
 for package in $@
 do
     # check package is already installed or not
     dnf list installed $package &>>$LOG_FILE
 
     # if exit status is 0, already installed. -ne 0 need to install it
-    if [ $? -ne o ]; then
+    if [ $? -ne 0 ]; then
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "$package"
     else
         echo -e "$package already installed ... $Y SKIPPING $N"
     fi
-done 
+done
