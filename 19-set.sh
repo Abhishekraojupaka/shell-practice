316f5f7ba4917b7e7efb74b8e856dc4b4c3f439e
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-28 15:13:27 +0530

    variable

--- 19-set.sh
+++ 19-set.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 set -e 
+
+error(){
+    echo "there is an error"
+}
+trap error ERR 
+
+
 echo "Hello.."
 echo "Before error.."
-sjaoioan
+sjaoioan;dnf # Here shell understands there is an error and signal is ERR
 echo "after error"
