5b9278aa613c7790abbf797a1f051c3e73837cea
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-10-05 00:12:55 +0530

    variable

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -20,11 +20,9 @@ if [ ! -d $SOURCE_DIR ]; then
     exit 1
 fi
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*log" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*log" type f -mtime +14)
 
 while IFS= read -r filepath
 do
-    echo "Deleting the file: $filepath"
-    rm -rf $filepath
     echo "Deleted the file: $filepath"
 done <<< $FILES_TO_DELETE
